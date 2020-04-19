a1e622b836c0f8f9d14af62710c8af36d8b3a316
cameliapatilea <cami.patilea@gmail.com>
2020-04-19 20:34:51 +0300

    Base game

--- src/helpers/joc.py
+++ src/helpers/joc.py
@@ -2,6 +2,7 @@ class Joc:
     lin = 0
     col = 0
     gol = '_'
+    jucator = ''
 
     # initializarea tablei
     def __init__(self, lin, col):
@@ -10,6 +11,76 @@ class Joc:
         self.tabla = [None] * lin
         for i in range(0, lin):
             self.tabla[i] = [self.gol] * col
+
+    def modifica_tabla(self, juc, i, j):
+        if i > self.lin or i < 0 or j > self.col or j < 0:
+            print("Locul ales nu este corect.")
+            return False
+
+        if self.tabla[i][j] != "_":
+            print("Locul ales este deja ocupat")
+            return False
+        self.tabla[i][j] = juc
+        if i == 0:
+            if j == 0:
+                self.tabla[i][j + 1] = "#"
+                self.tabla[i+1][j] = "#"
+                self.tabla[i+1][j+1] = "#"
+            elif j == self.col - 1:
+                self.tabla[i][j-1] = "#"
+                self.tabla[i+1][j] = "#"
+                self.tabla[i+1][j-1] = "#"
+            else:
+                self.tabla[i][j+1] = "#"
+                self.tabla[i+1][j+1] = "#"
+                self.tabla[i+1][j] = "#"
+                self.tabla[i+1][j-1] = "#"
+                self.tabla[i][j-1] = "#"
+        elif i == self.lin - 1:
+            if j == 0:
+                self.tabla[i][j + 1] = "#"
+                self.tabla[i-1][j] = "#"
+                self.tabla[i-1][j+1] = "#"
+            elif j == self.col - 1:
+                self.tabla[i][j-1] = "#"
+                self.tabla[i-1][j] = "#"
+                self.tabla[i-1][j-1] = "#"
+            else:
+                self.tabla[i][j+1] = "#"
+                self.tabla[i-1][j+1] = "#"
+                self.tabla[i-1][j] = "#"
+                self.tabla[i-1][j-1] = "#"
+                self.tabla[i][j-1] = "#"
+        else:
+            if j == 0:
+                self.tabla[i][j+1] = "#"
+                self.tabla[i-1][j] = "#"
+                self.tabla[i+1][j] = "#"
+                self.tabla[i-1][j+1] = "#"
+                self.tabla[i+1][j + 1] = "#"
+            elif j == self.col -1:
+                self.tabla[i][j - 1] = "#"
+                self.tabla[i - 1][j] = "#"
+                self.tabla[i - 1][j - 1] = "#"
+                self.tabla[i + 1][j - 1] = "#"
+                self.tabla[i + 1][j] = "#"
+            else:
+                self.tabla[i][j - 1] = "#"
+                self.tabla[i][j + 1] = "#"
+                self.tabla[i - 1][j] = "#"
+                self.tabla[i + 1][j] = "#"
+                self.tabla[i-1][j-1] = "#"
+                self.tabla[i-1][j+1] = "#"
+                self.tabla[i + 1][j-1] = "#"
+                self.tabla[i+1][j+1] = "#"
+        return True
+
+    def verifica_tabla(self):
+        for x in range(0, self.lin):
+            for y in range(0, self.col):
+                if self.tabla[x][y] == '_':
+                    return True
+        return False
 
     def __str__(self):
         matrice = ""
@@ -25,4 +96,3 @@ class Joc:
             matrice = matrice + linie
         return matrice
 
-    
--- src/obstruction.py
+++ src/obstruction.py
@@ -1,11 +1,33 @@
-from src.helpers.joc import *
-from src.helpers.arg_helper import *
+from helpers.joc import *
+from helpers.arg_helper import *
 import sys
 
 
+def input_mutari():
+    print("Introduceti linia si coloana unde doriti sa fie pus simbolul pt a juca")
+    print("Linie = ")
+    newLin = int(input())
+    print("Coloana = ")
+    newCol = int(input())
+    return newLin, newCol
+
+
 if __name__ == '__main__':
+
     linie, coloana, juc, dif, alg, gui = get_arguments(sys.argv[1:])
     linie = int(linie)
     coloana = int(coloana)
+    ok = False
+    print("Ati ales jucatorul " + juc)
     tabla = Joc(linie, coloana)
     print(str(tabla))
+
+    while tabla.verifica_tabla() is True:
+        pozX, pozY = input_mutari()
+
+        while tabla.modifica_tabla(juc, pozX, pozY) is False:
+            print("Introduceti din nou coordonatele")
+            pozX, pozY = input_mutari()
+        print(str(tabla))
+
+
